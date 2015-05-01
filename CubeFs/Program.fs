3fbb72b604fa151bba532eabcf09a52b863f7f9e
Aurelien Souchet <souchet.aurelien@gmail.com>
2015-05-02 00:00:48 +0200

    start adding types

---
+++ CubeFs/Program.fs
@@ -0,0 +1,30 @@
+﻿type Face = 
+    | U
+    | R
+    | F
+    | D
+    | L
+    | B
+
+type Color = 
+    | Blue
+    | Red 
+    | Green
+    | Orange
+    | Yellow
+    | White
+
+type PartInfo = (Color * Face)
+
+type CubePart = 
+    | Center of (PartInfo)
+    | Edge of (PartInfo * PartInfo)
+    | Corner of (PartInfo * PartInfo * PartInfo)
+    
+type Cube = CubePart list
+
+
+[<EntryPoint>]
+let main argv = 
+
+    0
